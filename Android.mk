9002fd58b5db911dfcca9c211b8851d846f19f85
Jonas Cardoso <jonascard60@gmail.com>
2016-06-30 10:55:35 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright 2016 The Android Open Source Project
+# Copyright 2016 The Android Open Source Project 
 # Copyright 2016 TeamHacklG
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
